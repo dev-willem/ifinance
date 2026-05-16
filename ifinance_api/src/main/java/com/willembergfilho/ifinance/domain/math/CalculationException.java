3d3a25e4d6267621427634eadfae70227b0c5345
dev-willem <willembergfilho@gmail.com>
2026-05-16 20:29:17 -0300

    feat: add CalculationException for domain-level math errors

---
+++ ifinance_api/src/main/java/com/willembergfilho/ifinance/domain/math/CalculationException.java
@@ -0,0 +1,8 @@
+package com.willembergfilho.ifinance.domain.math;
+
+public class CalculationException extends RuntimeException {
+
+    public CalculationException(String message) {
+        super(message);
+    }
+}
