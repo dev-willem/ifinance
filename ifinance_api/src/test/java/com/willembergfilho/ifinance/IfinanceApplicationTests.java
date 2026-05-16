978095aabc85087d24e0aa5969bcc3803752d232
dev-willem <willembergfilho@gmail.com>
2026-05-16 20:34:49 -0300

    feat: add IfinanceApplicationTests smoke test loading application context

---
+++ ifinance_api/src/test/java/com/willembergfilho/ifinance/IfinanceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.willembergfilho.ifinance;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IfinanceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
