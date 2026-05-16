e24e3d06c7ea2f25e709bc8c8fa368088fdde462
dev-willem <willembergfilho@gmail.com>
2026-05-16 20:30:15 -0300

    feat: add CorsConfig allowing frontend origin with credentials

---
+++ ifinance_api/src/main/java/com/willembergfilho/ifinance/infrastructure/security/CorsConfig.java
@@ -0,0 +1,23 @@
+package com.willembergfilho.ifinance.infrastructure.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+public class CorsConfig {
+	@Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins("http://localhost:7000")
+                        .allowedMethods("*")
+                        .allowedHeaders("*")
+                        .allowCredentials(true);
+            }
+        };
+    }
+}
