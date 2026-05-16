eeec115dc812a29a53745e7b1ad830265fb9d6f6
dev-willem <willembergfilho@gmail.com>
2026-05-16 20:35:47 -0300

    feat: add ESLint config with Vue TypeScript and Prettier integration

---
+++ ifinance/eslint.config.mjs
@@ -0,0 +1,62 @@
+import pluginVue from 'eslint-plugin-vue'
+import tseslint from '@typescript-eslint/eslint-plugin'
+import tsParser from '@typescript-eslint/parser'
+import vueParser from 'vue-eslint-parser'
+
+export default [
+  {
+    ignores: [
+      'dist/**',
+      'node_modules/**',
+      'src/types/auto-imports.d.ts',
+      'src/types/components.d.ts',
+      '.husky/**',
+      'coverage/**',
+    ],
+  },
+  {
+    files: ['src/**/*.ts', 'src/**/*.tsx'],
+    languageOptions: {
+      parser: tsParser,
+      parserOptions: {
+        ecmaVersion: 'latest',
+        sourceType: 'module',
+      },
+    },
+    plugins: {
+      '@typescript-eslint': tseslint,
+    },
+    rules: {
+      ...tseslint.configs.recommended.rules,
+      '@typescript-eslint/no-explicit-any': 'error',
+      '@typescript-eslint/no-unused-vars': ['error', { argsIgnorePattern: '^_' }],
+      '@typescript-eslint/explicit-function-return-type': 'off',
+      '@typescript-eslint/explicit-module-boundary-types': 'off',
+    },
+  },
+  {
+    files: ['src/**/*.vue'],
+    languageOptions: {
+      parser: vueParser,
+      parserOptions: {
+        parser: tsParser,
+        ecmaVersion: 'latest',
+        sourceType: 'module',
+        extraFileExtensions: ['.vue'],
+      },
+    },
+    plugins: {
+      vue: pluginVue,
+      '@typescript-eslint': tseslint,
+    },
+    rules: {
+      ...pluginVue.configs.base.rules,
+      ...pluginVue.configs['vue3-essential'].rules,
+      ...pluginVue.configs['vue3-strongly-recommended'].rules,
+      'vue/multi-word-component-names': 'off',
+      'vue/require-default-prop': 'off',
+      'vue/no-unused-vars': 'error',
+      '@typescript-eslint/no-explicit-any': 'error',
+    },
+  },
+]
