fd8e892ace905a9bb70b89bb63d534f7473e3a9f
dev-willem <willembergfilho@gmail.com>
2026-05-16 20:27:41 -0300

    feat: add Spring Boot application entry point with main class

---
+++ ifinance_api/src/main/java/com/willembergfilho/ifinance/IfinanceApplication.java
@@ -0,0 +1,13 @@
+package com.willembergfilho.ifinance;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IfinanceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IfinanceApplication.class, args);
+	}
+
+}
